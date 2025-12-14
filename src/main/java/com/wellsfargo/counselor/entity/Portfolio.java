8e2d78bb3207e95a0a5b7d07a17a48c2514984c5
Tina Aswani <tinaaswani99@gmail.com>
2025-12-14 23:02:17 +0530

    init

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,34 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long portfolioId;
+
+    @OneToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    public Portfolio(Long portfolioId, Client client) {
+        this.portfolioId = portfolioId;
+        this.client = client;
+    }
+
+    public Portfolio() {}
+
+    public Long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+}
---
+++ src/main/java/com/wellsfargo/counselor/example/demo/entity/FinancialAdvisor.java
@@ -0,0 +1,42 @@
+package com.wellsfargo.counselor.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class FinancialAdvisor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long advisorId;
+
+    private String name;
+    private String email;
+
+    public FinancialAdvisor(Long advisorId, String name, String email) {
+        this.advisorId = advisorId;
+        this.name = name;
+        this.email = email;
+    }
+
+    public FinancialAdvisor() {}
+
+    public Long getAdvisorId() {
+        return advisorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
